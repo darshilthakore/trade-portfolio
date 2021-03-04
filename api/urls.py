bb51e5885c0cfccf1b505ba9fa12af9a70a448e7
Darshil Thakore <darshilthakore@gmail.com>
2021-03-04 20:33:01 +0530

    changed url configurations

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('trades', views.TradeList.as_view()),
+    path('trades/', views.TradeList.as_view()),
     path('trades/<int:pk>', views.TradeDetail.as_view()),
-    path('portfolio', views.PortfolioView.as_view()),
-    path('returns', views.ReturnsView.as_view()),
+    path('portfolio/', views.PortfolioView.as_view()),
+    path('returns/', views.ReturnsView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
